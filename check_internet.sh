9f49bab5e6ca1931c3b8071c971ac5339b0f43f5
kzawshein <kaungzaws@gmail.com>
2021-01-18 14:53:43 +0800

    Create check_internet.sh

---
+++ check_internet.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+
+function internet_on(){
+/bin/echo "1" > /sys/devices/platform/leds-gpio/leds/mr16\:green\:power/brightness
+/bin/echo "0" > /sys/devices/platform/leds-gpio/leds/mr16\:orange\:power/brightness
+}
+
+function internet_off(){
+/bin/echo "1" > /sys/devices/platform/leds-gpio/leds/mr16\:orange\:power/brightness
+/bin/echo "0" > /sys/devices/platform/leds-gpio/leds/mr16\:green\:power/brightness
+}
+
+ping -q -w 1 -c 1 8.8.8.8 && internet_on || internet_off
